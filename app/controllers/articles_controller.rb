e98fb53b10461c23c507fd8e55635efaf26389c7
Val-Nah9 <jj_kk@ukr.net>
2026-08-19 16:25:32 +0300

    Display plain parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,12 @@ class ArticlesController < ApplicationController
 	
 	def new
 	end
-
+	
 	def create
+		render plain: params[:article].inspect
+
+		# Раньше (до Rails 8) выводило сразу без пере-направления и без ошибки (в Rails 5)
+		redirect_to articles_path			# (Перенаправляем на страницу index)
 	end
 
 end
